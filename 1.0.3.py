3ca1fb921dea052582f4f5cb51ae95579872a45a
paqul <42283871+paqul@users.noreply.github.com>
2019-03-24 17:43:22 +0100

    Update and rename 1.0.2.py to 1.0.3.py
    Change resolution in "Search" window

--- 1.0.3.py
+++ 1.0.3.py
@@ -4,7 +4,8 @@ from tkinter import *
 mainwindow = Tk()
 mainwindow.title('Web-Scraper')
 
-frame_root = Frame(mainwindow).grid(row=0, column=0, sticky=N)
+frame_root = Frame(mainwindow)
+frame_root.grid(row=0, column=0, sticky=N)
 
 
 #-------------------Frame 0 0 ---------------------------#
@@ -19,13 +20,17 @@ frame_0_0.grid(row=0, column=0, columnspan=3)
 
 #-------------------Frame 0 1 ---------------------------#
 frame_0_1 = Frame(frame_root)
-frame_search = LabelFrame(frame_0_1, text="Search", relief="groove", width=50, height=50)
+frame_search = LabelFrame(frame_0_1, text="Search", relief="groove")
 
-button_test = Button(frame_search, text="searcher 0 1")
-button_test.grid()
 
-frame_search.grid()
-frame_0_1.grid(row=0, column=3, columnspan=3)
+entry_label = Label(frame_search, text="Search:")#, height=15, width=20)
+entry_label.grid(row=0, column=3, columnspan=1, sticky=E)
+
+entry_text = Entry(frame_search, width=25)
+entry_text.grid(row=0, column=4, columnspan=2, sticky=W,  pady=60)
+
+frame_search.grid(row=0, column=3, columnspan=3, padx=5, pady=5)
+frame_0_1.grid(row=0, column=3, columnspan=3)#, padx=10, pady=10)
 
 #-------------------Frame 2 0 ---------------------------#
 frame_2_0 = Frame(frame_root)
@@ -96,7 +101,7 @@ frame_3_0.grid(row=3, column=0, columnspan=15)
 #-------------------Frame Vertical ---------------------------#
 frame_0_7 = Frame(frame_root)
 
-empty_space_vertical = Label(frame_0_7, bg="blue", width=3, height=10)
+empty_space_vertical = Label(frame_0_7, bg="blue", width=3, height=30)
 empty_space_vertical.grid()
 
 frame_0_7.grid(row=0, column=7, rowspan=7)
